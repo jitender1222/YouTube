44eb7a9c0ca156e54f65e1cd23867adb8398af37
jitender singh <jeetusinghkanyal@gmail.com>
2023-02-13 12:53:16 +0530

    added actions

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -12,12 +12,15 @@ const appSlice=createSlice({
         // we have to make a actions
 
         toogleMenu:(state)=>{
-
             state.isMenuOpen=!state.isMenuOpen
         },
+
+        closeMenu:(state)=>{
+            state.isMenuOpen=false
+        }
     },
 })
 
 
-export const {toogleMenu}=appSlice.actions;
+export const {toogleMenu,closeMenu}=appSlice.actions;
 export default appSlice.reducer;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
 
 const YOUTUBE_API_KEY="AIzaSyAWREoFYEG_NGU5W2fumMssrBqbWLvfxjo";
 
-export const YOU_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+YOUTUBE_API_KEY;
+export const YOU_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=In&key="+YOUTUBE_API_KEY;
