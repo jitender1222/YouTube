181995a062431113da9d5ba7277100952c42634e
jitender singh <jeetusinghkanyal@gmail.com>
2023-02-13 10:12:05 +0530

    callig you tube api

--- src/components/VideoConatiner.js
+++ src/components/VideoConatiner.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { YOU_VIDEO_API } from '../utils/constant';
 
 const VideoConatiner = () => {
+
+  useEffect(()=>{
+    getVideos();
+  },[])
+
+  const getVideos=async()=>{
+    const data=await fetch(YOU_VIDEO_API);
+    const json=await data.json();
+    console.log(json);
+  }
   return (
-    <div>VideoConatiner</div>
+    <>
+    <div>
+      helo world
+    </div>
+    </>
   )
 }
 
