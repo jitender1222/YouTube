7b63125d43f1bb6f162aa8012ec60ee417c388a9
jitender singh <jeetusinghkanyal@gmail.com>
2023-02-13 13:00:27 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import './App.css';
 import Body from './components/Body';
 import Header from './components/Header';
 import store from './utils/store';
-import {Provider} from "react-redux"
+import {Provider} from "react-redux";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainContainer from './components/MainContainer';
+import WatchPage from './components/WatchPage';
+
+const appRouter=createBrowserRouter([
+  {
+    path:'/',
+    element:<Body />,
+    children:[
+      {
+        path:'/',
+        element:<MainContainer />
+      },
+      {
+        path:'watch',
+        element:<WatchPage /> 
+      }
+    ]
+  }
+])
 
 function App() {
   return (
@@ -10,7 +30,7 @@ function App() {
     <Provider store={store}>
     <div>
       <Header />
-      <Body />
+      <RouterProvider router={appRouter} />
 
       {/*   
         Header
