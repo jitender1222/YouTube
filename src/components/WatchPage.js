a99b743a6ba03562d752d6dc24c14abb400768a5
jitender singh <jeetusinghkanyal@gmail.com>
2023-02-13 12:48:58 +0530

    added watchPage

---
+++ src/components/WatchPage.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
+import { closeMenu } from '../utils/appSlice';
+
+const WatchPage = () => {
+
+  const [searchParams]=useSearchParams();
+
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(closeMenu());
+  },[])
+  return (
+    <div className='p-6 m-2'>
+    <iframe width="1000" 
+    height="500" 
+    src={"https://www.youtube.com/embed/"+searchParams.get("v")} 
+    title="YouTube video player" 
+    frameBorder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    allowFullScreen>
+    </iframe>
+    </div>
+  )
+}
+
+export default WatchPage;
